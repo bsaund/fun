8ae23649a4ea0f3467339466330d8613e9bf1305
Brad Saund <brad.saund@gmail.com>
2023-02-19 15:54:16 -0800

    Added support for additional letters

--- letter_reader/letter_reader.py
+++ letter_reader/letter_reader.py
@@ -6,6 +6,30 @@ import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 from contextlib import closing
 import subprocess
+from threading import Lock
+
+
+HELP_STR = '''
+Welcome to Letter Presser
+Press any letter to have a voice read it.
+Press esc to exit
+'''
+
+SPECIAL_KEY_MAP = {
+    pynput.keyboard.Key.backspace: "Backspace",
+    pynput.keyboard.Key.delete: "Delete",
+    pynput.keyboard.KeyCode.from_char('/'): "slash",
+    pynput.keyboard.KeyCode.from_char('*'): "asterix",
+    pynput.keyboard.Key.left: "left",
+    pynput.keyboard.Key.right: "right",
+    pynput.keyboard.Key.up: "up",
+    pynput.keyboard.Key.down: "down",
+    pynput.keyboard.Key.tab: "Tab",
+    pynput.keyboard.Key.caps_lock: "Caps Lock",
+    pynput.keyboard.KeyCode.from_char(','): "comma",
+    pynput.keyboard.Key.shift: "shift",
+    pynput.keyboard.Key.shift_r: "shift",
+}
 
 
 def gen_filepath(speech):
@@ -23,17 +47,24 @@ class LetterReader:
         self.keyboard_listener = pynput.keyboard.Listener(on_press=self.on_press)
         self.keyboard_listener.start()
         self.letter_to_speak = None
+        self.should_exit = False
+
 
     def run(self):
-        while self.letter_to_speak != 'esc':
+        while not self.should_exit:
             if self.letter_to_speak is not None:
                 self.speak(self.letter_to_speak)
                 self.letter_to_speak = None
         print("Exiting")
+        return
+
 
     def on_press(self, key):
+        if key == pynput.keyboard.Key.esc:
+            self.should_exit = True
+            print("Esc Pressed")
+            return
         self.letter_to_speak = key
-        print(f"set letter to speak as {key}")
 
     def generate_new_mp3(self, speech):
         if self.polly is None:
@@ -56,22 +87,29 @@ class LetterReader:
                 file.write(stream.read())
 
     def speak(self, key):
+        print(f"Speaking {key}")
         if key == pynput.keyboard.Key.esc:
             print("Raising exception")
             os.kill(os.getpid(), signal.SIGUSR1)
 
-        try:
-            key.char
-        except AttributeError:
-            return
+        if key in SPECIAL_KEY_MAP:
+            char = SPECIAL_KEY_MAP[key]
+        else:
+            try:
+                char = key.char
+            except AttributeError:
+                print("Exception!")
+                return
 
-        if not os.path.exists(gen_filepath(key.char)):
-            self.generate_new_mp3(key.char)
-
-        subprocess.Popen(['mpg123', '-q', gen_filepath(key.char)]).wait()
+        if not os.path.exists(gen_filepath(char)):
+            self.generate_new_mp3(char)
+        fp = gen_filepath(char)
+        print(f"Opening file {fp}")
+        subprocess.Popen(['mpg123', '-q', '--no-control', fp]).wait()
 
 
 if __name__ == "__main__":
+    print(HELP_STR)
     lr = LetterReader()
     # lr.generate_new_mp3("a")
     lr.run()
