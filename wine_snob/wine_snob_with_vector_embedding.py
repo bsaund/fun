5f19fa1713c4404d6fa8848a68f62057ce93a5b6
Brad Saund <brad.saund@gmail.com>
2023-08-06 15:49:55 -0700

    Updated queries

--- wine_snob/wine_snob_with_vector_embedding.py
+++ wine_snob/wine_snob_with_vector_embedding.py
@@ -1,6 +1,27 @@
+"""
+This is a quick look at using vector embeddings to match input data with queries.
+To use this, you will set to set your environment variable for the openAI key
+https://github.com/openai/openai-python#usage
+"""
+
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from openai.embeddings_utils import get_embedding
+
+ENTRIES = [
+"The wine from the party in winter of 2016 had a picture of a dragon on the label. Eric said it was expensive.",
+"This wine was from 2018 and had a green label. It was okay, but Ive had better",
+"The merlot from the San Carlos diner is really great. It is a Chateau de Saund 2017. It paired really nicely with the fish",
+"The pinot noir from the San Carlos diner is awful. It is a Chateu de Cochran 1956. Never get this again"
+]
+
+QUERIES = [
+"I want the wine with the dragon label",
+"What was the wine I had from Chateau de Saund?",
+"I went to the San Carlos diner and had a great wine. What was it?",
+"Laura said some wine was really expensive. Or maybe it was Eric.",
+"I had a terrible wine at the San Carlos diner from some Chateu. What was it?"
+]
 
 def construct_ws_dataframe():
     df = pd.DataFrame(columns=["text", "vector_embedding"])
@@ -16,9 +37,8 @@ def insert_embedding_to_def(df, text):
     return df
 
 def wip_add_to_df(df):
-    df = insert_embedding_to_def(df, "The wine from the party in winter of 2016 had a picture of a dragon on the label. Eric said it was expensive.")
-    df = insert_embedding_to_def(df, "This wine was from 2018 and had a green label. It was okay, but Ive had better")
-    df = insert_embedding_to_def(df, "The merlot from the San Carlos diner is really great. It is a Chateau de Saund 2017. It paired really nicely with the fish")
+    for entry in ENTRIES:
+        df = insert_embedding_to_def(df, entry)
     return df
 
 
@@ -27,20 +47,17 @@ def vector_similarity(df, text):
     vec = embed(text)
     df["similarity"] = df["vector_embedding"].apply(lambda x: cosine_similarity([x], [vec]))
     df = df.sort_values(by=["similarity"], ascending=False).reset_index()
-    # print(df)
     return df.loc[0]
 
 
 def retrieve(df, text):
     most_relevant = vector_similarity(df, text)
-    print(f"You asked for: {text}")
-    print(f"The most relevent entry is: {most_relevant['text']}")
+    print(f"You asked for    :   {text}")
+    print(f"The best entry is:   {most_relevant['text']}")
     print()
 
 if __name__ == "__main__":
     ws_df = construct_ws_dataframe()
     wip_add_to_df(ws_df)
-    retrieve(ws_df, "I want the wine with the dragon label")
-    retrieve(ws_df, "What was the wine I had from Chateau de Saund?")
-    retrieve(ws_df, "I went to the San Carlos diner and had a great wine. What was it?")
-    retrieve(ws_df, "Laura said some wine was really expensive. Or maybe it was Eric.")
+    for query in QUERIES:
+        retrieve(ws_df, query)
